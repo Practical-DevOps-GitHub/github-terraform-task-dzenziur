3cb0cf87a01db9c8c1a1b3d7ad9502392462d541
Vladyslav Dzenziur <dzenziur@gmail.com>
2024-04-13 20:02:36 +0200

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "repository_name" {
-  description = "GitHub repository name"
-  default     = "github-terraform-task-dzenziur"
-}
-
-variable "softserve_user" {
-  description = "Default SofrServe user"
-  default     = "softservedata"
-}
