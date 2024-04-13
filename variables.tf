fdf3797f407a0789b96ffd112d167962a16eab94
Vladyslav Dzenziur <dzenziur@gmail.com>
2024-04-13 18:22:53 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,10 +3,6 @@ variable "repository_name" {
   default     = "github-terraform-task-dzenziur"
 }
 
-variable "SECRETS_TOKEN" {
-  description = "GitHub token"
-}
-
 variable "softserve_user" {
   description = "Default SofrServe user"
   default     = "softservedata"
